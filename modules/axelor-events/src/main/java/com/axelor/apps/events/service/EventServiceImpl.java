6a4fcdaf6e7707ef2ccd90b7f889d09e128b3068
Teng Yue <t.yue@axelor.com>
2023-11-17 10:09:52 -0500

    events module version 1.0.2 modified Associated To

--- modules/axelor-events/src/main/java/com/axelor/apps/events/service/EventServiceImpl.java
+++ modules/axelor-events/src/main/java/com/axelor/apps/events/service/EventServiceImpl.java
@@ -1,7 +1,11 @@
 package com.axelor.apps.events.service;
 
+import com.axelor.apps.contact.db.Contact;
+import com.axelor.apps.contact.db.repo.ContactRepository;
 import com.axelor.apps.events.db.Event;
 import com.axelor.apps.events.db.repo.EventRepository;
+import com.axelor.apps.sales.db.Product;
+import com.axelor.apps.sales.db.repo.ProductRepository;
 import com.axelor.inject.Beans;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
@@ -13,10 +17,14 @@ import java.time.LocalDateTime;
 public class EventServiceImpl implements EventService {
 
     protected EventRepository eventRepository;
+    protected ContactRepository contactRepository;
+    protected ProductRepository productRepository;
 
     @Inject
-    public EventServiceImpl(EventRepository eventRepository) {
+    public EventServiceImpl(EventRepository eventRepository, ContactRepository contactRepository, ProductRepository productRepository) {
         this.eventRepository = eventRepository;
+        this.contactRepository = contactRepository;
+        this.productRepository = productRepository;
     }
 
     @Transactional
@@ -62,5 +70,28 @@ public class EventServiceImpl implements EventService {
         Event eventObject = Beans.get(EventRepository.class).find(event.getId());
         eventObject.setStatus(3);
         eventObject.setEndDate(null);
+    }
+
+    @Transactional
+    @Override
+    public void setAssociatedTo(Event event) {
+        Event eventObject = Beans.get(EventRepository.class).find(event.getId());
+        String type = eventObject.getAssociatedToSelect();
+        Long objectId = eventObject.getAssociatedToSelectId();
+        if("com.axelor.apps.sales.db.Product".equals(type)){
+            Product product = Beans.get(ProductRepository.class).find(objectId);
+            product.addEventListItem(eventObject);
+            eventObject.setProduct(product);
+            productRepository.save(product);
+            eventRepository.save(eventObject);
+        }
+        else{
+            Contact contact = Beans.get(ContactRepository.class).find(objectId);
+            contact.addEventListItem(eventObject);
+            eventObject.setContact(contact);
+            contactRepository.save(contact);
+            eventRepository.save(eventObject);
+        }
+
     }
 }
--- modules/axelor-events/src/main/java/com/axelor/apps/events/web/EventController.java
+++ modules/axelor-events/src/main/java/com/axelor/apps/events/web/EventController.java
@@ -62,21 +62,31 @@ public class EventController {
     public void startDateOnChange(ActionRequest request, ActionResponse response) {
         Event event = request.getContext().asType(Event.class);
         LocalDateTime startDate = event.getStartDate();
-        if(startDate != null){
-            response.setValue("status",1);
-        }else{
-            response.setValue("status",0);
+        LocalDateTime endDate = event.getEndDate();
+        if (startDate != null && endDate == null) {
+            response.setValue("status", 1);
+        }
+        if (startDate == null) {
+            response.setValue("status", 0);
+            response.setValue("endDate", null);
         }
     }
 
     public void endDateOnChange(ActionRequest request, ActionResponse response) {
         Event event = request.getContext().asType(Event.class);
         LocalDateTime endDate = event.getEndDate();
-        if(endDate != null){
-            response.setValue("status",2);
-        }else{
-            response.setValue("status",1);
+        if (endDate != null) {
+            response.setValue("status", 2);
+        } else {
+            response.setValue("status", 1);
         }
+    }
+
+    public void setAssociatedTo(ActionRequest request, ActionResponse response) {
+        Event event = request.getContext().asType(Event.class);
+        EventService eventService = Beans.get(EventService.class);
+        eventService.setAssociatedTo(event);
+        response.setReload(true);
     }
 
 
